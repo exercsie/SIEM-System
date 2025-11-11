ec482cf8bbb2eb652f08bfff8f169e33dbe0cf43
exercsie <hammondtristan62@gmail.com>
2025-11-12 02:26:31 +1100

    Added a vector so that the program can handle multiple incoming events, also added for loops to constantly display messages depending on the amount of events available

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <vector>
 #include "Logger/logger.h"
 
 int main() {
-    Event event = {"login", "bobkalamr721", "192.398.382.18", false};
-    Rule rule = {"Failed login detected", "success", "==", "false", "High"};
+    std::vector<Event> events = {
+        {"login", "bonnieMaster922", "192.168.32.2", false},
+        {"login", "basketball199", "192.168.92.3", false},
+        {"login", "mouse12", "192.168.3.22", false}
+    };
 
-    if (evaluate(event, rule)) {
-        Alert alert = generateAlert(event, rule);
-        std::cout << "[" + alert.severity << "]" << alert.message << "\n";
-    } else {
-        std::cout << "No alert triggered.\n";
+    std::vector<Rule> rules = {
+        {"Failed login detected", "success", "==", "false", "High"}
+    };
+
+    for (const auto &event : events) {
+        for (const auto &rule : rules) {
+            if (evaluate(event, rule)) {
+                Alert alert = generateAlert(event, rule);
+                std::cout << "[" << alert.severity << "]" << alert.message << "\n";
+            }
+        }
     }
 
     return 0;
