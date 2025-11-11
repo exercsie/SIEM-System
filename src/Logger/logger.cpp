93f28135daefb74e34bd3872c66f5f8041acbb36
exercsie <hammondtristan62@gmail.com>
2025-11-12 02:04:20 +1100

    Created basic alert system for the SIEM that will display the severity of the attack, what they tried to do, the username, and IP of the user.

---
+++ src/Logger/logger.cpp
@@ -0,0 +1,18 @@
+#include "logger.h"
+#include <iostream>
+
+bool evaluate(const Event &e, const Rule &r) {
+    if (r.field == "success" && r.op == "==") {
+        bool val = (r.value == "true");
+        return e.success == val;
+    }
+    return false;
+}
+
+Alert generateAlert(const Event &e, Rule &r) {
+    Alert a;
+    a.name = r.name;
+    a.severity = r.severity;
+    a.message = "Alert: " + r.name + " User: " + e.username + " IP: " + e.src_ip;
+    return a;
+}
