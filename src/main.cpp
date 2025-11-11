c3aa400df00609f2fae51b8c6400f6fb40b6ced7
exercsie <hammondtristan62@gmail.com>
2025-11-12 02:41:43 +1100

    Put the processing logic into it's own function

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,7 @@
 #include <vector>
 #include "Logger/logger.h"
 
-int main() {
-    std::vector<Event> events = {
-        {"login", "bonnieMaster922", "192.168.32.2", false, "High"},
-        {"login", "basketball199", "192.168.92.3", false, "Low"},
-        {"login", "mouse12", "192.168.3.22", false, "Medium"}
-    };
-
-    std::vector<Rule> rules = {
-        {"Failed login detected", "success", "==", "false"}
-    };
-
+void processEvents(const std::vector<Event> &events, const std::vector<Rule> &rules) {
     for (const auto &event : events) {
         for (const auto &rule : rules) {
             if (evaluate(event, rule)) {
@@ -21,6 +11,20 @@ int main() {
             }
         }
     }
+}
+
+int main() {
+    std::vector<Event> events = {
+        {"login", "bonnieMaster922", "192.168.32.2", true, "High"},
+        {"login", "basketball199", "192.168.92.3", false, "Low"},
+        {"login", "mouse12", "192.168.3.22", false, "Medium"}
+    };
+
+    std::vector<Rule> rules = {
+        {"Failed login detected", "success", "==", "false"}
+    };
+
+    processEvents(events, rules);
 
     return 0;
 }
