f26300fa677d9fbac77a3e6d5f4ccc3ec927c585
user8s <hammondtristan62@gmail.com>
2025-11-19 14:48:12 +1100

    Added a check for comparing unsafe IPs

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <vector>
 #include "Logger/logger.h"
+#include "Logger/IPList.h"
 
-void processEvents(const std::vector<Event> &events, const std::vector<Rule> &rules) {
+void processEvents(const std::vector<Event> &events, const std::vector<Rule> &rules, const IPList &unsafeIPs) {
     for (const auto &event : events) {
+        if (unsafeIPs.contains(event.src_ip)) {
+            std::cout << "[!!] ALERET: Unsafe IP detected " << event.src_ip << " (User: " << event.username << ")\n";
+        }
         for (const auto &rule : rules) {
             if (evaluate(event, rule)) {
                 Alert alert = generateAlert(event, rule);
@@ -15,7 +19,7 @@ void processEvents(const std::vector<Event> &events, const std::vector<Rule> &ru
 
 int main() {
     std::vector<Event> events = {
-        {"login", "bonnieMaster922", "192.168.32.2", true, "High"},
+        {"login", "bonnieMaster922", "192.168.32.2", false, "High"},
         {"login", "basketball199", "192.168.92.3", false, "Low"},
         {"login", "mouse12", "192.168.3.22", false, "Medium"}
     };
